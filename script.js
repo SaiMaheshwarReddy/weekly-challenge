2812409174ee4cde02d9a4f9d3e68f1449c5e17a
SaiMaheshwarReddy <61627080+SaiMaheshwarReddy@users.noreply.github.com>
2021-03-22 16:25:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const IMAGE = 'https://image.tmdb.org/t/p/w500';
 
 const API_KEY = 'd8bf019d0cca372bd804735f172f67e8';
 
+
+
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/multi?api_key=d8bf019d0cca372bd804735f172f67e8&language=en-US&query=';
 
+
+
 const header = document.querySelector(".header")
+const container = document.querySelector(".container")
 const moviesContainer = document.querySelector(".movies__container");
+const modalBox = document.querySelector(".modal__box");
 
 const searchInput = document.getElementById("search__input");
-const searchBtn = document.getElementById("search__btn");
+const form = document.getElementById("form");
+const modalContainer = document.querySelector(".modal__container");
+const movieBoxBtn = document.querySelector(".movie__box");
+const closeModal = document.getElementById("closeModal");
 
 const selectMovieBox = document.querySelector(".movie__box")
 var searchTerm;
@@ -16,17 +25,30 @@ var searchTerm;
 const trendingUrl= 'https://api.themoviedb.org/3/trending/all/day?api_key=d8bf019d0cca372bd804735f172f67e8';
 
 
-
+// https://image.tmdb.org/t/p/w500/uQtqiAu2bBlokqjlURVLEha6zoi.jpg
     
- 
+ //modal
+
+
+
+
 
 showTrendingMovies(trendingUrl)
 
-searchBtn.addEventListener("click", handleClick)
+form.addEventListener("submit",
+(e)=>{
+   e.preventDefault();
+    handleClick();
+}
+)
 
-function handleClick(e){
+
+
+
+
+function handleClick(){
    
-    e.preventDefault()
+   
     header.textContent=  searchInput.value;
       moviesContainer.innerHTML = '';
     if(searchInput.value == "" || searchInput.value == " " || searchInput.value == "  " || searchInput.value == "   " || searchInput.value == "    "){
@@ -45,7 +67,7 @@ function handleClick(e){
 async function getMovies(url) {
     const response = await fetch(url);
     const data = await response.json();
-   
+ 
     fetchedData(data)
    
 
@@ -58,12 +80,10 @@ async function getMovies(url) {
 function fetchedData(data) {
   
     const results = data.results;
-    results.forEach(getInfo)
-
-}
-
-function getInfo(movie){
-const imgPath = IMAGE + movie.poster_path;
+    console.log(results)
+    let output = '';
+    results.forEach((movie) => {
+        const imgPath = IMAGE + movie.poster_path;
 const title = movie.title;
 if(title === undefined){
     moviesContainer.classList.remove('movie__box')
@@ -72,94 +92,126 @@ if(title === undefined){
 }
 
 else{
+    
+
+    output += `
+    <div class="movie__box" onclick= "showMovieInfo(${movie.id})">
+    <div class="img__box">
+        <img src=${imgPath} alt="">
+    </div>
+    <div class="movie__box__text">
+        <p class="title">${title}</p>
+        </div>
+        </div>
+    `;
+ 
+    moviesContainer.innerHTML = output
 
 
-const movieBox = document.createElement("div");
-movieBox.setAttribute("class", "movie__box");
-   const htmlElements = `
-   <div class="img__box">
-       <img src=${imgPath} alt="">
-   </div>
-   <div class="movie__box__text">
-       <p class="title">${title}</p><span class="ratings">${movie.vote_average}</span>
-   `;
-   movieBox.innerHTML = htmlElements;
-   moviesContainer.appendChild(movieBox);
   
 }
 
+    })
+
 }
+
+
 
 async function showTrendingMovies(tUrl) {
     const response = await fetch(tUrl);
     const data = await response.json();
+    // console.log(data.results)
    fetchTrending(data)
-   
+//    setTimeout( movieInfo(data.results), 2000);
+  
 
 }
 
 function fetchTrending(data) {
     const trendingMovieList = data.results
-    trendingMovieList.forEach(showTrending)
+let output = '';
+    trendingMovieList.forEach(movie => {
+        const imgPath = IMAGE + movie.poster_path;
+        const title = movie.title;
+        if(title === undefined){
+            moviesContainer.classList.remove('movie__box')
+        } else if(movie.poster_path === null) {
+            moviesContainer.classList.remove('movie__box')
+         }
+        
+        
+        else{
+        
+          output += `
+           <div class="movie__box" onclick= "showMovieInfo(${movie.id})">
+           <div class="img__box">
+               <img src=${imgPath} alt="">
+           </div>
+           <div class="movie__box__text">
+               <p class="title">${title}</p>
+               </div>
+               </div>
+           `;
+        
+           moviesContainer.innerHTML = output
+        
+        }
+    })
+
+
+console.log(trendingMovieList)
+    
 }
 
 
 
-function showTrending(movie) {
-    const imgPath = IMAGE + movie.poster_path;
-const title = movie.title;
-if(title === undefined){
-    moviesContainer.classList.remove('movie__box')
-} else if(movie.poster_path === null) {
-    moviesContainer.classList.remove('movie__box')
- }
-
-
-else{
-
-
-const movieBox = document.createElement("div");
-movieBox.setAttribute("class", "movie__box");
-   const htmlElements = `
-   <div class="img__box">
-       <img src=${imgPath} alt="">
-   </div>
-   <div class="movie__box__text">
-       <p class="title">${title}</p><span class="ratings">${movie.vote_average}</span>
-   `;
-   movieBox.innerHTML = htmlElements;
-   moviesContainer.appendChild(movieBox);
-  
-}
-}
-
-
-
-
-// function showInfo() {
-//     console.log("movie box")
+// function showTrending(movie, output) {
+   
 // }
 
 
 
 
+
+function showMovieInfo(id) {
+    modalContainer.classList.toggle("appear");
+    console.log(id)
+ let eachMovieURL = 'https://api.themoviedb.org/3/movie/' + id +  '?api_key=d8bf019d0cca372bd804735f172f67e8&language=en-US'
+ 
+
+    fetch(eachMovieURL).then(res => res.json())
+    .then(data => {
+        console.log(data)
+let modalinfo = '';
+let backdrop = IMAGE + data.backdrop_path;
+modalinfo += `
+<div class="modal__wrapper">
+<i class="fas fa-times" " id="closeModal" onclick= "closeModalFunc()"></i>
+<div class="img">
+    <img src=${backdrop} alt="">
+</div>
+<h2 class="modal__title">
+${data.title}
+   
+</h2>
+<span><b>Rating: </b>${data.vote_average}</span>
+<p>${data.overview}</p>
+</div>
+`
+
+modalContainer.innerHTML = modalinfo;
    
 
 
+    }).catch(err => console.log(err))
 
+}
+// setTimeout(()=> {
+//     closeModal.addEventListener("click", ()=> {
+       
+//     })
+// }, 2000)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function closeModalFunc() {
+    modalContainer.classList.toggle("appear");
+}
